5ae21a0d5e70f4ea1a243692522c1e2ee760d2e1
Lisonallie <allisonvanlinden@gmail.com>
2019-12-18 16:45:25 +0100

    add method to clear data after checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,6 +9,7 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
   constructor(
     private cartService: CartService,
@@ -17,6 +18,18 @@ export class CartComponent implements OnInit {
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    //Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
